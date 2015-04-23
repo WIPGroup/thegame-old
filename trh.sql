a6b5e795f69035743c0b3b7545dc0ae3afb19280
Jiří Vozár <jiri.vozar@gmail.com>
2015-04-23 19:10:05 +0200

    Update databáze.

--- trh.sql
+++ trh.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Počítač: localhost
--- Vytvořeno: Čtv 23. dub 2015, 08:28
+-- Vytvořeno: Čtv 23. dub 2015, 19:09
 -- Verze serveru: 5.5.43-0ubuntu0.14.10.1
 -- Verze PHP: 5.5.12-2ubuntu4.4
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `hraci` (
-`id` int(11) NOT NULL,
+`idhrace` int(11) NOT NULL,
   `jmeno` varchar(50) NOT NULL,
   `heslo` varchar(100) NOT NULL,
   `vlastnictvi` varchar(100) NOT NULL
@@ -37,10 +37,10 @@ CREATE TABLE IF NOT EXISTS `hraci` (
 -- Vypisuji data pro tabulku `hraci`
 --
 
-INSERT INTO `hraci` (`id`, `jmeno`, `heslo`, `vlastnictvi`) VALUES
-(1, 'root', 'root', '100;12;8'),
+INSERT INTO `hraci` (`idhrace`, `jmeno`, `heslo`, `vlastnictvi`) VALUES
+(1, 'root', 'root', '76;20;8'),
 (2, 'test1', 'test1', '100;0;0'),
-(3, 'test2', 'test2', '100;0;0');
+(3, 'test2', 'test2', '94;0;0');
 
 -- --------------------------------------------------------
 
@@ -49,41 +49,46 @@ INSERT INTO `hraci` (`id`, `jmeno`, `heslo`, `vlastnictvi`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `obchod` (
-`id` int(11) NOT NULL,
+`idnab` int(11) NOT NULL,
   `hrac` int(11) NOT NULL,
   `predmet` int(11) NOT NULL,
   `mnozstvi` int(11) NOT NULL,
-  `penize` int(11) NOT NULL,
+  `cena` int(11) NOT NULL,
   `smer` varchar(1) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Vypisuji data pro tabulku `obchod`
 --
 
-INSERT INTO `obchod` (`id`, `hrac`, `predmet`, `mnozstvi`, `penize`, `smer`) VALUES
+INSERT INTO `obchod` (`idnab`, `hrac`, `predmet`, `mnozstvi`, `cena`, `smer`) VALUES
 (1, 1, 1, 4, 10, 'p'),
-(2, 1, 1, 2, 6, 'k');
+(2, 1, 1, 2, 6, 'k'),
+(3, 1, 1, 1, 1, 'p'),
+(6, 1, 1, 1, 4, 'p'),
+(7, 1, 1, 1, 2, 'k'),
+(8, 3, 1, 1, 1, 'p'),
+(9, 3, 1, 1, 1, 'k'),
+(10, 3, 1, 1, 1, 'k');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `polozky`
+-- Struktura tabulky `veci`
 --
 
-CREATE TABLE IF NOT EXISTS `polozky` (
-`id` int(11) NOT NULL,
-  `nazev` varchar(50) NOT NULL,
-  `cena` varchar(100) NOT NULL
+CREATE TABLE IF NOT EXISTS `veci` (
+`idveci` int(11) NOT NULL,
+  `nazev` varchar(50) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- Vypisuji data pro tabulku `polozky`
+-- Vypisuji data pro tabulku `veci`
 --
 
-INSERT INTO `polozky` (`id`, `nazev`, `cena`) VALUES
-(1, 'Dřevo', '10'),
-(2, 'Písek', '5');
+INSERT INTO `veci` (`idveci`, `nazev`) VALUES
+(1, 'Dřevo'),
+(2, 'Písek');
 
 --
 -- Klíče pro exportované tabulky
@@ -93,19 +98,19 @@ INSERT INTO `polozky` (`id`, `nazev`, `cena`) VALUES
 -- Klíče pro tabulku `hraci`
 --
 ALTER TABLE `hraci`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD KEY `id_2` (`id`);
+ ADD PRIMARY KEY (`idhrace`), ADD UNIQUE KEY `id` (`idhrace`), ADD KEY `id_2` (`idhrace`);
 
 --
 -- Klíče pro tabulku `obchod`
 --
 ALTER TABLE `obchod`
- ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`);
+ ADD PRIMARY KEY (`idnab`), ADD KEY `id` (`idnab`);
 
 --
--- Klíče pro tabulku `polozky`
+-- Klíče pro tabulku `veci`
 --
-ALTER TABLE `polozky`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD KEY `id_2` (`id`);
+ALTER TABLE `veci`
+ ADD PRIMARY KEY (`idveci`), ADD UNIQUE KEY `id` (`idveci`), ADD KEY `id_2` (`idveci`);
 
 --
 -- AUTO_INCREMENT pro tabulky
@@ -115,17 +120,17 @@ ALTER TABLE `polozky`
 -- AUTO_INCREMENT pro tabulku `hraci`
 --
 ALTER TABLE `hraci`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `idhrace` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pro tabulku `obchod`
 --
 ALTER TABLE `obchod`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `idnab` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
--- AUTO_INCREMENT pro tabulku `polozky`
+-- AUTO_INCREMENT pro tabulku `veci`
 --
-ALTER TABLE `polozky`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+ALTER TABLE `veci`
+MODIFY `idveci` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
