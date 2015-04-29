e5da9656a891f819e80fd98fa0be77553b212659
Jiří Vozár <jiri.vozar@gmail.com>
2015-04-29 16:54:58 +0000

    test

--- hrac_menu.php
+++ hrac_menu.php
@@ -1,7 +1,7 @@
 <ul class="nav navbar-nav navbar-right" style="padding-right: 15px">
         <li><a href="https://gitlab.com/AntreTeam/TheGame">GitLab</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo 'Username'; //TODO: PHP magick ?><span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo 'Username'; //TODO: PHP magick - jirvoz saw this ?><span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="#">Profil</a></li>
             <li><a href="#">Trade History</a></li>
