1e90821b875ae401805be87c67adcb216df93d9d
Jozef Hollý <j2.00ghz@gmail.com>
2015-04-29 21:32:16 +0200

    Cheange buttons in table

--- trhupdate/predavanie.php
+++ trhupdate/predavanie.php
@@ -3,20 +3,20 @@ session_start();
 include "../connectvlastnictvi.php";
 include "../trade.php";
 echo '<table style="background-color: #fff" class="table table-bordered table-responsive table-hover">';
-echo '<tr><th>Hráč</th><th>Ponúka</th><th>Množstvo</th><th>Cena</th><th></th></tr>';
+echo '<tr><th>Hráč</th><th>Ponúka</th><th>Množstvo</th><th>Cena</th><th></th><th></th></tr>';
 $dotaz = 'SELECT * FROM obchod, veci, hraci WHERE smer="p" AND predmet=idveci AND hrac=idhrace';
 $vysledek = mysql_query($dotaz) or die(mysql_error($db));
 while ($zaznam = mysql_fetch_array($vysledek)) {
-  echo '<tr';
-  if ($zaznam['cena'] > $vlastnictvi[0])
-    echo ' class="danger"';
-  echo '<tr><td>' . $zaznam['jmeno'] . '</td>';
-  echo '<td>' . $zaznam['nazev'] . '</td>';
-  echo '<td>' . $zaznam['mnozstvi'] . '</td>';
-  echo '<td>' . $zaznam['cena'] . '</td>';
-  echo '<td>';
-  if ($zaznam['cena'] <= $vlastnictvi[0]) echo '<button type="button" class="btn btn-success" href="#" onclick="obchodovanie(' . $zaznam['idnab'] . ');return false;">Kúpiť</button>';//TODO: misto a chceme button
-  echo '<button type="button" class="btn btn-warning" href="#" onclick="drop(' . $zaznam['idnab'] . ');return false;">Zrušiť</button></td></tr>';
+	echo '<tr';
+	if ($zaznam['cena'] > $vlastnictvi[0])
+		echo ' class="danger"';
+	echo '<tr><td>' . $zaznam['jmeno'] . '</td>';
+	echo '<td>' . $zaznam['nazev'] . '</td>';
+	echo '<td>' . $zaznam['mnozstvi'] . '</td>';
+	echo '<td>' . $zaznam['cena'] . '</td>';
+	echo '<td>';
+	if ($zaznam['cena'] <= $vlastnictvi[0]) echo '<button type="button" class="btn btn-success btn-block" href="#" onclick="obchodovanie(' . $zaznam['idnab'] . ');return false;">Kúpiť</button></td><td>';//TODO: misto a chceme button
+	echo '<button type="button" class="btn btn-warning btn-block" href="#" onclick="drop(' . $zaznam['idnab'] . ');return false;">Zrušiť</button></td></tr>';
 }
 echo '</table>';
 ?>
