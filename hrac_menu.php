b31936fab21dfe76d8d8455f2197b25c4817d4ed
Jozef Hollý <j2.00ghz@gmail.com>
2015-04-29 19:48:09 +0200

    logout button vystredit a taby misto mezer

--- hrac_menu.php
+++ hrac_menu.php
@@ -1,13 +1,19 @@
 <ul class="nav navbar-nav navbar-right" style="padding-right: 15px">
-        <li><a href="https://gitlab.com/AntreTeam/TheGame">GitLab</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo 'Username'; //TODO: PHP magick ?><span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Profil</a></li>
-            <li><a href="#">Trade History</a></li>
-            <li><a href="#">Statistics</a></li>
-            <li class="divider"></li>
-            <li><form action="logout.php" class="navbar-form navbar-right"><button type="submit" class="btn btn-lg btn-danger btn-block">Odhlásiť</button></form></li>
-          </ul>
-        </li>
-      </ul>
+				<li><a href="https://gitlab.com/AntreTeam/TheGame">GitLab</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo 'Username'; //TODO: PHP magick ?><span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Profil</a></li>
+						<li><a href="#">Trade History</a></li>
+						<li><a href="#">Statistics</a></li>
+						<li class="divider"></li>
+						<li>
+							<form action="logout.php" class="navbar-form">
+								<div style="text-align: center">
+									<button type="submit" class="btn btn-lg btn-danger btn-block">Odhlásiť</button>
+								</div>
+							</form>
+						</li>
+					</ul>
+				</li>
+			</ul>
