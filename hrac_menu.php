327c0c6b0eb5868dfb39f7f6f978656e18f5361b
Jozef Hollý <j2.00ghz@gmail.com>
2015-04-29 18:04:36 +0200

    #18 fix invisible user

--- hrac_menu.php
+++ hrac_menu.php
@@ -1,19 +1,13 @@
 <ul class="nav navbar-nav navbar-right">
-  <li class="dropdown">
-    <ul class="dropdown-menu" role="menu">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-        <?php
-          echo 'Username'; //TODO: PHP magick
-        ?>
-        <span class="caret"></span>
-      </a>
-      <ul class="dropdown-menu" role="menu">
-        <li><a href="#">Profil</a></li>
-        <li><a href="#">Recent trades</a></li>
-        <li><a href="#">Something other</a></li>
-        <li class="divider"></li>
-        <li><form action="logout.php" class="navbar-form navbar-right"><button type="submit" class="btn btn-lg btn-danger btn-block">Odhlásiť</button></form></li>
-  </ul>
-    </ul>
-  </li>
-</ul>
+        <li><a href="https://gitlab.com/AntreTeam/TheGame">GitLab</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo 'Username'; //TODO: PHP magick ?><span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Profil</a></li>
+            <li><a href="#">Trade History</a></li>
+            <li><a href="#">Statistics</a></li>
+            <li class="divider"></li>
+            <li><a href="#"><form action="logout.php" class="navbar-form navbar-right"><button type="submit" class="btn btn-lg btn-danger btn-block">Odhlásiť</button></form></a></li>
+          </ul>
+        </li>
+      </ul>
