4da0a5494940ef2adfa47b2f9cfbc204a9b27195
Jozef Hollý <j2.00ghz@gmail.com>
2015-04-29 16:47:00 +0200

    #18

--- index.php
+++ index.php
@@ -10,7 +10,9 @@ session_start();
     <?php
     require "dblogin.php";
     require "login.php";
-    if (isset($_GET['trade']) || isset($_GET[''])) echo '<meta http-equiv="refresh" content="0; url=index.php">';
+    if (isset($_GET['trade']) || isset($_GET[''])) {
+        echo '<meta http-equiv="refresh" content="0; url=index.php">';
+    }
     ?>
     <script src="https://code.jquery.com/jquery-1.11.2.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
@@ -24,17 +26,22 @@ session_start();
           TheGame
         </a>
       </div>
-      <a class="navbar-link" href='https://ci.gitlab.com/projects/2263?ref=master'><img src='https://ci.gitlab.com/projects/2263/status.png?ref=master' /></a>
-      <a class="navbar-link" href='https://ci.gitlab.com/projects/2263?ref=latest-working'><img src='https://ci.gitlab.com/projects/2263/status.png?ref=latest-working' /></a>
-      <?php
-      if ($prihlasen)
-      echo '<form action="logout.php" class="navbar-form navbar-right"><button type="submit" class="btn btn-lg btn-danger btn-block">Odhlásiť</button></form>'
-      ?>
-      <p class="navbar-text navbar-right">Prihlásený ako (user)</p><!--TODO: PHP magic-->
+      <ul class="nav navbar-nav">
+      <li><a class="navbar-link" href='https://ci.gitlab.com/projects/2263?ref=master'><img src='https://ci.gitlab.com/projects/2263/status.png?ref=master' /></a></li>
+      <li><a class="navbar-link" href='https://ci.gitlab.com/projects/2263?ref=latest-working'><img src='https://ci.gitlab.com/projects/2263/status.png?ref=latest-working' /></a></li></ul>
+        <?php
+        if ($prihlasen) {
+            echo '<ul class="nav navbar-nav navbar-right"><li><form action="logout.php" class="navbar-form navbar-right"><button type="submit" class="btn btn-lg btn-danger btn-block">Odhlásiť</button></form></li>';
+        }
+        ?>
+      <li><p class="navbar-text navbar-right">Prihlásený ako (user)</p></li></ul><!--TODO: PHP magic-->
     </nav>
     <?php
-    if ($prihlasen) include "trh.php";
-    else include "form.php";
+    if ($prihlasen) {
+        include "trh.php";
+    } else {
+        include "form.php";
+    }
     ?>
   </body>
 </html>
