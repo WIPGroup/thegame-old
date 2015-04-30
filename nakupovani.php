f61d4224afc3c9dacd562d902f6be01024c335cd
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-01 00:56:45 +0200

    nakupovani fix

--- nakupovani.php
+++ nakupovani.php
@@ -13,39 +13,51 @@ include "trade.php";
 <div class="col-md-2">
 	<form id="nabidka">
 		<div class="form-group">
-			<table><tr><td>
-				<div class="input-group panel panel-primary">
-					<div class="panel-heading">Nabízím:</div>
-					<div class="panel-body">
-						<select name="nabizi" id="nabizi" class="form-control">
-							<?php
-								echo $selectitems;
-								?>
-						</select>
-						<input type="number" name="mnoznabizi" id="mnoznabizi" min="1" max="1000" value="1" class="form-control" placeholder="Množstvo">
-					</div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h2 class="panel-title">Zadat objednávku:</h2>
 				</div>
-			</td><td>
-				<div class="input-group panel panel-primary">
-					<div class="panel-heading">Prodam:</div>
-					<div class="panel-body">
-						<select name="chce" id="chce" class="form-control">
-							<?php
-							echo $selectitems;
-							?>
-						</select>
-					<input type="number" name="mnozchce" id="mnozchce" min="1" max="1000" value="1" class="form-control" placeholder="Množstvo">
-					</div>
+				<div class="panel-body">
+					<table>
+						<tr>
+							<td>
+								<div class="input-group panel panel-primary">
+									<div class="panel-heading">Nabízím:</div>
+									<div class="panel-body">
+										<select name="nabizi" id="nabizi" class="form-control">
+											<?php
+												echo $selectitems;
+											?>
+										</select>
+										<input type="number" name="mnoznabizi" id="mnoznabizi" min="1" max="1000" value="1" class="form-control" placeholder="Množstvo">
+									</div>
+								</div>
+							</td>
+							<td>
+								<div class="input-group panel panel-primary">
+									<div class="panel-heading">Prodam:</div>
+									<div class="panel-body">
+										<select name="chce" id="chce" class="form-control">
+											<?php
+												echo $selectitems;
+											?>
+										</select>
+										<input type="number" name="mnozchce" id="mnozchce" min="1" max="1000" value="1" class="form-control" placeholder="Množstvo">
+									</div>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2">
+								<button type="submit" class="btn btn-primary btn-block">Odoslať</button>
+							</td>
+						</tr>
+					</table>
 				</div>
-			</td></tr>
-			<tr>
-				<td colspan="2">
-					<button type="submit" class="btn btn-primary btn-block">Odoslať</button>
-				</td>
-			</tr>
-			</table>
+			</div>
 		</div>
 	</form>
-	<div id="vlastnictvi"></div>
+	<div id="vlastnictvi">
+	</div>
 </div>
 <span id="nabidky" class="col-md-10"></span>
