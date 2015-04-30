b6085eb3438f7bc5d31eb68c897a67bbcb245b2e
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-01 01:48:32 +0200

    fix multiple buttons

--- trhupdate/nabidky.php
+++ trhupdate/nabidky.php
@@ -40,10 +40,14 @@ while ($zaznam = mysql_fetch_array($vysledek))
 	if ($zaznam['hrac'] == $_SESSION['hrac']) {
 		echo '<button type="button" class="btn btn-warning btn-block" href="#" onclick="cancel(' . $zaznam['idnab'] . ');return false;">Zrušiť</button>';
 	}
-	echo '<button type="button" class="btn btn-success btn-block" href="#" onclick="obchodovanie(' . $zaznam['idnab'] . ');return false;" ';
-	if ($zaznam['idhrace'] != $_SESSION['hrac'] && $zaznam['mnozchce'] > $vlastnictvi[$zaznam['chce']])
-		echo 'disabled ';
-	echo ">Kúpiť</button></td></tr>";
+	else if ($zaznam['mnozchce'] <= $vlastnictvi[$zaznam['chce']]) {
+		echo '<button type="button" class="btn btn-success btn-block" href="#" onclick="obchodovanie(' . $zaznam['idnab'] . ');return false;" ';
+		if ($zaznam['idhrace'] != $_SESSION['hrac'] && $zaznam['mnozchce'] > $vlastnictvi[$zaznam['chce']]) {
+			echo 'disabled ';
+		}
+		echo '>Kúpiť</button>';
+	}
+	echo "</td></tr>";
 }
 echo '</table>';
 ?>
