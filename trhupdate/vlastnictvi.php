0ea67266023b6c72d33bbac86678fbf1333673b4
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-01 01:41:05 +0200

    vlastnictvi table panel

--- trhupdate/vlastnictvi.php
+++ trhupdate/vlastnictvi.php
@@ -1,12 +1,11 @@
 <?php
-session_start();
-include "../vlastnictvi.php";
-echo '<div><table style="background-color: #fff" class="table table-bordered table-responsive table-hover">';
-echo '<tr><td>Peniaze</td><td>'.$vlastnictvi[0].'</td></tr>';
-$dotaz = 'SELECT * FROM veci';
-$vysledek = mysql_query($dotaz) or die(mysql_error($db));
-while ($zaznam = mysql_fetch_array($vysledek)) {
-  echo '<tr><td>'.$zaznam['nazev'].'</td><td>'.$vlastnictvi[$zaznam['idveci']].'</td></tr>';
-}
-echo '</table></div>';
+	session_start();
+	include "../vlastnictvi.php";
+	echo '<div class="panel panel-default"><div class="panel-heading"><h2 class="panel-title">Inventář:</h2></div><div class="panel-body"><table style="background-color: #fff" class="table table-bordered table-responsive table-hover"><tr><td>Peniaze</td><td>'.$vlastnictvi[0].'</td></tr>';
+	$dotaz = 'SELECT * FROM veci';
+	$vysledek = mysql_query($dotaz) or die(mysql_error($db));
+	while ($zaznam = mysql_fetch_array($vysledek)) {
+		echo '<tr><td>'.$zaznam['nazev'].'</td><td>'.$vlastnictvi[$zaznam['idveci']].'</td></tr>';
+	}
+	echo '</table></div></div>';
 ?>
