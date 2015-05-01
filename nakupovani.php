2af9a4fd36595ad5966fc18a3f32541edbc4d245
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-02 01:43:48 +0200

    change grid

--- nakupovani.php
+++ nakupovani.php
@@ -10,8 +10,8 @@ include "trade.php";
 		$selectitems .= '<option value="'.$zaznam['idveci'].'">'.$zaznam['nazev'].'</option>'."\n";
 	}
 ?>
-<div class="col-lg-3 col-md-12" id="objednavka">
-	<div class="col-lg-12 col-md-6">
+<div class="col-lg-3 col-md-12 col-sm-12 col-xs-12" id="objednavka">
+	<div class="col-lg-12 col-md-6 col-sm-6 col-xs-12">
 		<form id="nabidka">
 			<div class="form-group">
 				<div class="panel panel-default">
@@ -19,50 +19,38 @@ include "trade.php";
 						<h2 class="panel-title">Zadat objednávku:</h2>
 					</div>
 					<div class="panel-body">
-						<table>
-							<tr>
-								<td>
-									<div class="input-group panel panel-primary">
-										<div class="panel-heading">Nabízím:</div>
-										<div class="panel-body">
-											<select name="nabizi" id="nabizi" class="form-control">
-												<?php
-													echo $selectitems;
-												?>
-											</select>
-											<input type="number" name="mnoznabizi" id="mnoznabizi" min="1" max="1000" value="1" class="form-control" placeholder="Množstvo">
-										</div>
-									</div>
-								</td>
-								<td>
-									<div class="input-group panel panel-primary">
-										<div class="panel-heading">Prodam:</div>
-										<div class="panel-body">
-											<select name="chce" id="chce" class="form-control">
-												<?php
-													echo $selectitems;
-												?>
-											</select>
-											<input type="number" name="mnozchce" id="mnozchce" min="1" max="1000" value="1" class="form-control" placeholder="Množstvo">
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2">
-									<button type="submit" class="btn btn-primary btn-block">Odoslať</button>
-								</td>
-							</tr>
-						</table>
+						<div class="input-group panel panel-primary col-lg-6 col-md-6 col-sm-12 col-xs-12">
+							<div class="panel-heading">Nabízím:</div>
+							<div class="panel-body">
+								<select name="nabizi" id="nabizi" class="form-control">
+								<?php
+									echo $selectitems;
+									?>
+								</select>
+								<input type="number" name="mnoznabizi" id="mnoznabizi" min="1" max="1000" value="1" class="form-control" placeholder="Množstvo">
+							</div>
+						</div>
+						<div class="input-group panel panel-primary col-lg-6 col-md-6 col-sm-12 col-xs-12">
+							<div class="panel-heading">Hledám:</div>
+							<div class="panel-body">
+								<select name="chce" id="chce" class="form-control">
+								<?php
+									echo $selectitems;
+									?>
+								</select>
+								<input type="number" name="mnozchce" id="mnozchce" min="1" max="1000" value="1" class="form-control" placeholder="Množstvo">
+							</div>
+						</div>
+						<button type="submit" class="btn btn-primary btn-block">Odoslať</button>
 					</div>
 				</div>
 			</div>
 		</form>
 	</div>
-	<div class="col-lg-12 col-md-6" id="vlastnictvi">
+	<div class="col-lg-12 col-md-6 col-sm-6 col-xs-12" id="vlastnictvi">
 	</div>
 </div>
-<div class="panel panel-default col-lg-9 col-lg-offset-3 col-md-12">
+<div class="panel panel-default col-lg-9 col-lg-offset-3 col-md-12 col-sm-12 col-xs-12">
 	<div class="panel-heading">
 		<h2 class="panel-title">Tržiště:</h2>
 	</div>
