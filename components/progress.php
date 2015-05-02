f337f4d83cbc90850a521080e74ac98a30b39899
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-02 16:34:16 +0200

    fix research mobile

--- components/progress.php
+++ components/progress.php
@@ -1,4 +1,4 @@
-<div class="col-lg-12 col-md-3 col-sm-4 col-xs-12">
+<div class="col-lg-12 col-md-4 col-sm-6 col-xs-12">
 	<div class="panel panel-default">
 		<div class="panel-heading">
 			<h1 class="panel-title">Stav</h1>
