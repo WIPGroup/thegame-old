63056174184071453daa3a1c6d021fa1e75c7a50
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-02 15:02:57 +0200

    add research

---
+++ components/progress.php
@@ -0,0 +1,5 @@
+<div class="progress col-lg-12 col-md-3 col-sm-4 col-xs-12">
+	<div class="progress-bar progress-bar-striped active" role="progressbar" style="width: 100%">
+		Počítače pracují
+	</div>
+</div>
---
+++ components/researchtree.php
@@ -0,0 +1,8 @@
+<div class="panel panel-default col-lg-9 col-xs-12">
+	<div class="panel-heading">
+		<h1 class="panel-title">Strom výzkumu</h1>
+	</div>
+	<div class="panel-body">
+		zde bude strom výzkumu
+	</div>
+</div>
