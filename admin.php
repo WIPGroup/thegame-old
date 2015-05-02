95c20206bfd973f530822dc80d103b34830287b5
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-02 18:21:52 +0200

    make admin page

---
+++ admin.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+		require "dblogin.php";
+		require "login.php";
+		include "components/head.php"
+		?>
+	</head>
+	<body>
+		<?php
+		include "components/navbar.php";
+		if ($prihlasen)
+		{
+			include "log.php";
+		}
+		else
+		{
+			include "components/form.php";
+		}
+		?>
+	</body>
+</html>
