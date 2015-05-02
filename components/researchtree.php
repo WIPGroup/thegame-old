09c2978e86246641f4a7b4d6096d9b02aa7b9a24
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-02 23:39:29 +0200

    research tree layout

--- components/researchtree.php
+++ components/researchtree.php
@@ -1,8 +1,10 @@
-<div class="panel panel-default col-xs-12">
-	<div class="panel-heading">
-		<h1 class="panel-title">Strom výzkumu</h1>
-	</div>
-	<div class="panel-body">
-		zde bude strom výzkumu
+<div class="col-xs-12">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h1 class="panel-title">Strom výzkumu</h1>
+		</div>
+		<div class="panel-body">
+			zde bude strom výzkumu
+		</div>
 	</div>
 </div>
