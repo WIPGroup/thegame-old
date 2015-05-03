bad6381b39d0e46d7ff86e4629815a709c556603
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-03 11:58:41 +0200

    google analytics (reverted from commit 440c65b71a286102299d26d07745a1016372278b)

--- components/analyticstracking.php
+++ components/analyticstracking.php
@@ -1,6 +1,5 @@
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('set', '&uid', '<?php $dotaz = 'SELECT jmeno FROM hraci WHERE idhrace='.$_SESSION['hrac']; $vysledek = mysql_query($dotaz) or die(mysql_error($db));	$zaznam = mysql_fetch_array($vysledek);	echo $zaznam['jmeno'];?>');
 ga('create', 'UA-62034614-3', 'auto');
 ga('send', 'pageview');
 </script>
