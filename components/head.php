6cc8c37c20f2f147586343b1a119200e68636d7d
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-03 12:32:37 +0200

    main table slovak

--- components/head.php
+++ components/head.php
@@ -10,3 +10,4 @@
 <!-- DataTables -->
 <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
 <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/plug-ins/1.10.7/i18n/Slovak.json"></script>
