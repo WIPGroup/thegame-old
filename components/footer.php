49ce938b14c1e3574178afaae551bd8aa7bf4de5
opuro <vaclav.janc@gmail.com>
2015-05-03 15:48:22 +0200

    Přidávání JS souboru

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,6 @@
 <script src="js/scripts.js"></script>
+<?php
+	echo '<script src="js/'.preg_replace('/\.php$/', '', __FILE__).'.js"></script>'
+?>
 </body>
 </html>
--- trh.php
+++ trh.php
@@ -9,6 +9,5 @@ if ($prihlasen){
 } else {
 	include "components/form.php";
 }
-require "js/index.js";
 require "components/footer.php";
 ?>
