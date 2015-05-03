7554bc750b3c74d645c9a654b32a1936e2fa3a46
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-03 14:04:14 +0200

    fix tooltips

--- scripts.js
+++ scripts.js
@@ -1,44 +1,44 @@
 function reloadInv()                //obnoveni tabulky vlastnictvi
 {
 	$.ajax({
-        url : "trhupdate/vlastnictvi.php", //vykona se to co je v url
-        success : function (data) {  //prijdou zpatky nejake data
+		url : "trhupdate/vlastnictvi.php", //vykona se to co je v url
+		success : function (data) {  //prijdou zpatky nejake data
 			$("#vlastnictvi").html(data);  //data se hodi do neceho s id vlastnictvi, easy
-        }
-    });
+		}
+	});
 }
 function reloadNabidky()
 {
-    $.ajax({
-        url : "trhupdate/nabidky.php",
-        success : function (data) {
-            $("#nabidky").html(data);
-        }
-    });
+	$.ajax({
+		url : "trhupdate/nabidky.php",
+		success : function (data) {
+			$("#nabidky").html(data);
+		}
+	});
 }
 function obchodovanie(idnab){
- 	$.get( "trh.php", { trade: idnab } ).done(reloadEverything());  //pri kliknuti na cudlik koupit nebo prodat se posle get request na index.php s parametry trade=idnab, potom se reloadne interface
+	$.get( "trh.php", { trade: idnab } ).done(reloadEverything());  //pri kliknuti na cudlik koupit nebo prodat se posle get request na index.php s parametry trade=idnab, potom se reloadne interface
 }
 function cancel(idnab){
- 	$.get( "trh.php", { cancel: idnab } ).done(reloadEverything()); //to same jen cudlik Zrušiť
+	$.get( "trh.php", { cancel: idnab } ).done(reloadEverything()); //to same jen cudlik Zrušiť
 }
 function reloadEverything(){  //reloadnuti interface s delayem 100ms kvuli rychlosti zpracovani pozadavku
 	setTimeout(function(){
-	reloadNabidky();
-	reloadInv();
+		reloadNabidky();
+		reloadInv();
 	},100);
 }
 $(function() { //odeslani formulare s nabidkou
-    $('#nabidka').submit(function() { 
-        $.ajax({ 
-            data: $(this).serialize(), //odesle se to co je vybrane jako klasicka get metoda, vybrane hodnoty se prevedou na tentyz string, jako kdyby to byl normalni submit
-            type: "GET", 
-            url: "trh.php", 
-            success: function(data) { 
+	$('#nabidka').submit(function() {
+		$.ajax({
+			data: $(this).serialize(), //odesle se to co je vybrane jako klasicka get metoda, vybrane hodnoty se prevedou na tentyz string, jako kdyby to byl normalni submit
+			type: "GET",
+			url: "trh.php",
+			success: function(data) {
 				reloadEverything(); //po odeslani se znovu nacte interface
-            }
-        });
+			}
+		});
 		return false;  //zastavi normalni submit, tj. zadny refresh
-    });
+	});
 });
 $(reloadEverything); //po nacteni stranky se nacte interface, easy enough
--- trhupdate/nabidky.php
+++ trhupdate/nabidky.php
@@ -118,4 +118,7 @@ $(document).ready(function (){
 		});
 	});
 });
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+})
 </script>
