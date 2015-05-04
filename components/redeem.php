b5f7054099df41cdf6bda7aad39d5c2032b87c47
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-04 13:38:22 +0200

    Index translations

--- components/redeem.php
+++ components/redeem.php
@@ -1,17 +1,17 @@
 <div class="col-xs-12">
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h1 class="panel-title">Log</h1>
+      <h1 class="panel-title">Uplatnění surovinného kupónu</h1>
     </div>
     <div class="panel-body" style="width: 100%; heigth: 100%">
       <form class="form-inline">
         <div class="form-group">
           <div class="input-group">
-            <div class="input-group-addon">Redeem</div>
+            <div class="input-group-addon">Kód kupónu:</div>
             <input type="text" class="form-control" id="exampleInputAmount" placeholder="ABCDEFGH">
           </div>
         </div>
-        <button type="submit" class="btn btn-primary">Transfer cash</button>
+        <button type="submit" class="btn btn-primary">Uplatnit surovinný kupón</button>
       </form>
     </div>
   </div>
