ee5afee7166e792dac731c9ca7e741a46bfacdb2
opuro <vaclav.janc@gmail.com>
2015-05-04 16:32:54 +0200

    DataTableFix

--- js/trh.js
+++ js/trh.js
@@ -1,27 +1,4 @@
 var aktualniTab = 'main';
-var slovencina = {
-			"sEmptyTable":     "Nie sú k dispozícii žiadne dáta",
-			"sInfo":           "Záznamy _START_ až _END_ z celkom _TOTAL_",
-			"sInfoEmpty":      "Záznamy 0 až 0 z celkom 0 ",
-			"sInfoFiltered":   "(vyfiltrované spomedzi _MAX_ záznamov)",
-			"sInfoPostFix":    "",
-			"sInfoThousands":  ",",
-			"sLengthMenu":     "Zobraz _MENU_ záznamov",
-			"sLoadingRecords": "Načítavam...",
-			"sProcessing":     "Spracúvam...",
-			"sSearch":         "Hľadať:",
-			"sZeroRecords":    "Nenašli sa žiadne vyhovujúce záznamy",
-			"oPaginate": {
-				"sFirst":    "Prvá",
-				"sLast":     "Posledná",
-				"sNext":     "Nasledujúca",
-				"sPrevious": "Predchádzajúca"
-			},
-			"oAria": {
-				"sSortAscending":  ": aktivujte na zoradenie stĺpca vzostupne",
-				"sSortDescending": ": aktivujte na zoradenie stĺpca zostupne"
-			}
-		};
 $(function() { //odeslani formulare s nabidkou
 	$('#nabidka').submit(function() {
 		$.ajax({
