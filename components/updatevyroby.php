1cf8e09db2b04248895ea8d433aaa92266655b72
Jiří Vozár <jiri.vozar@gmail.com>
2015-05-04 19:40:52 +0200

    Crafting po dokončení už přiděluje suroviny.

--- components/updatevyroby.php
+++ components/updatevyroby.php
@@ -1,12 +1,12 @@
 <?php
-	$dotaz = 'SELECT * FROM vyroba WHERE hrac='.$_SESSION['hrac'];
+	$dotaz = 'SELECT * FROM vyroba, recepty WHERE recept=idreceptu AND hrac='.$_SESSION['hrac'];
 	$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 	while ($zaznam = mysql_fetch_array($vysledek))
 	{
 		if ($zaznam['hotovo'] <= time())
 		{
 			//přičíst suroviny
-			$vlastnictvi[$zaznam['vyrobek']] ++;
+			$vlastnictvi[$zaznam['vyrobek']]++;
 			$dotaz = 'UPDATE hraci SET vlastnictvi="'.join(';', $vlastnictvi).'" WHERE idhrace="'.$_SESSION['hrac'].'"';
 			mysql_query($dotaz);
 			//smazat výrobu
