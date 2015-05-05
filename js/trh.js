8d6804a0a6486954d320c3dc00356bacd14b7ad4
opuro <vaclav.janc@gmail.com>
2015-05-05 18:03:47 +0200

    Rebuild plox

--- js/trh.js
+++ js/trh.js
@@ -6,10 +6,10 @@ $(function() { //odeslani formulare s nabidkou
 			type: "GET",
 			url: "trh.php",
 			success: function(data) {
-				reloadTrh(); //po odeslani se znovu nacte interface
+				reloadTrh(); //po odeslani se nacte interface
 			}
 		});
 		return false;  //zastavi normalni submit, tj. zadny refresh
 	});
 });
-$(reloadTrh()); //po nacteni stranky se nacte interface, easy enough
+$(reloadTrh()); //po nacteni stranky se nacte interface, easy 
