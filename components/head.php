4a0d638d8aefad20cdf4de3afef75773915198b9
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-05 18:42:52 +0200

    spacelab theme (reverted from commit 82b897926f3094701137cc5c0b3f4ce2181ac741)

--- components/head.php
+++ components/head.php
@@ -14,9 +14,6 @@
 	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
 	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-	<!-- Bootstrap spacelab theme -->
-	<link rel="stylesheet" href="bower_components/bootswatch-dist/css/bootstrap.min.css">
-	<script src="bower_components/bootswatch-dist/js/bootstrap.min.js"></script>
 	<!-- jQuery -->
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 	<!-- DataTables -->
