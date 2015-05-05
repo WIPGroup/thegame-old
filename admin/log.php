c1b3b45b3abc8e714e6c349a4b05f5f887131563
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-05 22:06:53 +0200

    peknejsi log snad

--- admin/log.php
+++ admin/log.php
@@ -4,16 +4,17 @@
 			<h1 class="panel-title">Log</h1>
 		</div>
 		<div class="panel-body" style="width: 100%; heigth: 100%; text-align:left;">
-			<?php
-			$dotaz = 'SELECT * FROM log, hraci WHERE hrac=idhrace ORDER BY cas DESC LIMIT 100';
-			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
-			while ($zaznam = mysql_fetch_array($vysledek))
-			{
-				echo "[".date("Y.m.d H:i:s",$zaznam['cas'])."][".$zaznam['jmeno']."]: ".$zaznam['text'];
-				
-				echo "<br>\n";
-			}
-			?>
+			<ul class="list-group">
+				<?php
+				$dotaz = 'SELECT * FROM log, hraci WHERE hrac=idhrace ORDER BY cas DESC LIMIT 100';
+				$vysledek = mysql_query($dotaz) or die(mysql_error($db));
+				while ($zaznam = mysql_fetch_array($vysledek))
+				{
+					echo '<li class="list-group-item">['.date("Y.m.d H:i:s",$zaznam['cas'])."][".$zaznam['jmeno']."]: ".$zaznam['text'];
+					echo "</li>";
+				}
+				?>
+			</ul>
 		</div>
 	</div>
 </div>
