20f25e06bcaed7753bf033f2953c3d2ebb45bae0
opuro <vaclav.janc@gmail.com>
2015-05-05 07:30:01 +0200

    Hotfix syntaxe

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ function cancel(idnab){
 	$.get( "trh.php", { cancel: idnab } ).done(reloadEverything()); //to same jen cudlik Zrušiť
 }
 function craft(idreceptu){
-	$.get( "components/craft.php", { craft: idreceptu } ).done(reloadVyroba();reloadInv();); //to samé jen tlačítko vyrobit
+	$.get( "components/craft.php", { craft: idreceptu } ).done(reloadVyroba()); //to samé jen tlačítko vyrobit
 }
 function reloadEverything(){ 
 	reloadNabidky();
