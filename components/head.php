996c200c249514d8ddc5e6e19a77b48fafd5a60a
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-05 19:06:01 +0200

    spacelab theme (reverted from commit 82b897926f3094701137cc5c0b3f4ce2181ac741) (reverted from commit 4a0d638d8aefad20cdf4de3afef75773915198b9) theme revert revert, head reorder

--- components/head.php
+++ components/head.php
@@ -3,6 +3,8 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>TheGame</title>
+	<!-- jQuery -->
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 	<!-- Pace -->
 	<link href="bower_components/pace/themes/blue/pace-theme-barber-shop.css" rel="stylesheet" />
 	<script src="bower_components/pace/pace.js"></script>
@@ -14,8 +16,9 @@
 	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
 	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-	<!-- jQuery -->
-	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<!-- Bootstrap spacelab theme -->
+	<link rel="stylesheet" href="bower_components/bootswatch-dist/css/bootstrap.min.css">
+	<script src="bower_components/bootswatch-dist/js/bootstrap.min.js"></script>
 	<!-- DataTables -->
 	<link rel="stylesheet" type="text/css" href="bower_components/datatables/media/css/jquery.dataTables.min.css">
 	<script type="text/javascript" charset="utf8" src="bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
