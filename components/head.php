b48ffdb9e61bc6f83693f4eaf7244d9077e18b32
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-06 22:11:20 +0200

    add npm (reverted from commit 05fd8493af319471dbe6fb98f2a41e81816668dc) fix

--- components/head.php
+++ components/head.php
@@ -4,26 +4,26 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>TheGame</title>
 	<!-- jQuery -->
-	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 	<!-- Pace -->
-	<link href="node_modules/pace/themes/blue/pace-theme-minimal.css" rel="stylesheet" />
-	<script src="node_modules/pace/pace.js"></script>
+	<link href="bower_components/pace/themes/blue/pace-theme-minimal.css" rel="stylesheet" />
+	<script src="bower_components/pace/pace.js"></script>
 	<!-- Offline -->
-	<link href="node_modules/offline/themes/offline-theme-slide.css" rel="stylesheet" />
-	<link href="node_modules/offline/themes/offline-language-english.css" rel="stylesheet" />
-	<script src="node_modules/offline/offline.min.js"></script>
+	<link href="bower_components/offline/themes/offline-theme-slide.css" rel="stylesheet" />
+	<link href="bower_components/offline/themes/offline-language-english.css" rel="stylesheet" />
+	<script src="bower_components/offline/offline.min.js"></script>
 	<!-- Bootstrap -->
-	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-	<!--<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-theme.min.css"> bootstrap original theme-->
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<!--<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css"> bootstrap original theme-->
 	<!-- Bootstrap spacelab theme -->
-	<link rel="stylesheet" href="node_modules/bootswatch-dist/css/bootstrap.min.css">
-	<script src="node_modules/bootswatch-dist/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="bower_components/bootswatch-dist/css/bootstrap.min.css">
+	<script src="bower_components/bootswatch-dist/js/bootstrap.min.js"></script>
 	<!-- DataTables -->
-	<link rel="stylesheet" type="text/css" href="node_modules/datatables/media/css/jquery.dataTables.min.css">
-	<script type="text/javascript" charset="utf8" src="node_modules/datatables/media/js/jquery.dataTables.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="bower_components/datatables/media/css/jquery.dataTables.min.css">
+	<script type="text/javascript" charset="utf8" src="bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
 	<!-- SweetAlert -->
-	<script src="node_modules/sweetalert/lib/sweet-alert.min.js"></script>
-	<link rel="stylesheet" href="node_modules/sweetalert/lib/sweet-alert.css">
+	<script src="bower_components/sweetalert/lib/sweet-alert.min.js"></script>
+	<link rel="stylesheet" href="bower_components/sweetalert/lib/sweet-alert.css">
 	<!-- Nase veci -->
 	<link rel="stylesheet" href="main.css">
 	<link rel="shortcut icon" href="favicon.ico">
