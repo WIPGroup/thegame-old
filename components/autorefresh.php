f64802533dd50b70c6b9e22a51bd5d9f4d11e020
opuro <vaclav.janc@gmail.com>
2015-05-06 16:00:07 +0200

    Opravení refresh tlačítka

--- components/autorefresh.php
+++ components/autorefresh.php
@@ -1,128 +1,46 @@
 <div id="RefreshBlockDesktop" class="pull-right phone-hide">
 	<div class="btn-toolbar" id="RefreshBlock">
 		<div class="btn-group">
-			<button class="btn btn-inverse" id="RefreshButton"><i class="icon-refresh" id="RefreshAnimation" style="-webkit-transform: rotate(180deg); transform: rotate(180deg);"></i></button>
+			<button class="btn btn-inverse" id="refreshButton"><span class="glyphicon glyphicon-refresh"></span></button>
 			<button class="btn btn-inverse dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
-			<ul class="dropdown-menu menu-refresh" id="RefreshMenu" style="left: -80px;">
+			<ul class="dropdown-menu menu-refresh" id="refreshMenu">
 				<li class="menu-header">Automatic refresh</li>
-				<li data="0.1">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td><i class="icon-ok"></i></td>
-									<td>0.1</td>
-									<td>Second</td>
-								</tr>
-							</tbody>
-						</table>
-					</a>
-				</li>
-				<li data="0.2">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td></td>
-									<td>0.2</td>
-									<td>Second</td>
-								</tr>
-							</tbody>
-						</table>
-					</a>
-				</li>
-				<li data="0.5">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td></td>
-									<td>0.5</td>
-									<td>Second</td>
-								</tr>
-							</tbody>
-						</table>
-					</a>
-				</li>
 				<li data="1">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td></td>
-									<td>1</td>
-									<td>Second</td>
-								</tr>
-							</tbody>
-						</table>
+					<a href="#">
+									1
+									Second
+
 					</a>
 				</li>
 				<li data="5">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td></td>
-									<td>5</td>
-									<td>Seconds</td>
-								</tr>
-							</tbody>
-						</table>
+					<a href="#">
+									5
+									Seconds
 					</a>
 				</li>
 				<li data="15">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td></td>
-									<td>15</td>
-									<td>Seconds</td>
-								</tr>
-							</tbody>
-						</table>
+					<a href="#">
+									15
+									Seconds
 					</a>
 				</li>
 				<li data="30">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td></td>
-									<td>30</td>
-									<td>Seconds</td>
-								</tr>
-							</tbody>
-						</table>
+					<a href="#">
+									30
+									Seconds
 					</a>
 				</li>
 				<li data="60">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td></td>
-									<td>1</td>
-									<td>Minute</td>
-								</tr>
-							</tbody>
-						</table>
+					<a href="#">
+									1
+									Minute
 					</a>
 				</li>
 				<li data="0">
-					<a href="#" class="has-table">
-						<table>
-							<tbody>
-								<tr>
-									<td></td>
-									<td style="width:0"></td>
-									<td>Off</td>
-								</tr>
-							</tbody>
-						</table>
+					<a href="#">
+									Off
 					</a>
 				</li>
-				<li data="mark" style="display: none;"><i class="icon-ok"></i></li>
 			</ul>
 		</div>
 	</div>
