31a2eed3658c8d60ff3f43b44a9ad193156d6944
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-06 20:23:40 +0200

    alignment fix

--- components/hrac_menu.php
+++ components/hrac_menu.php
@@ -8,13 +8,15 @@
 		</a>
 	</li>
 	<li><a href="https://gitlab.com/AntreTeam/TheGame">GitLab</a></li>
-	<p>
-		<?php
-		$dotaz = 'SELECT jmeno FROM hraci WHERE idhrace='.$_SESSION['hrac'];
-		$vysledek = mysql_query($dotaz) or die(mysql_error($db));
-		$zaznam = mysql_fetch_array($vysledek);
-		echo $zaznam['jmeno'];
-		?>
-	</p>
+	<li>
+		<p>
+			<?php
+			$dotaz = 'SELECT jmeno FROM hraci WHERE idhrace='.$_SESSION['hrac'];
+			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
+			$zaznam = mysql_fetch_array($vysledek);
+			echo $zaznam['jmeno'];
+			?>
+		</p>
+	</li>
 </li>
 </ul>
