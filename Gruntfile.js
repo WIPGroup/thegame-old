2028f16fc0720a2bb2e5496d1bcce526f6524240
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-07 17:25:47 +0200

    grunt clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,18 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-		all: ['**/*.js']
-	}
+		clean: {
+			js: ["bower_components/*.*",
+			"!bower_components/*.min.js",
+			"!bower_components/*.min.css",
+			"!bower_components/pace/themes/black/pace-theme-minimal.css",
+			"!bower_components/pace/pace.js",
+			"!bower_components/offline/themes/offline-theme-slide.css",
+			"!bower_components/offline/themes/offline-language-english.css",
+			"!bower_components/sweetalert/lib/sweet-alert.css"]
+		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', ['jshint']);
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.registerTask('default', ['clean']);
 };
