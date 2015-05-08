d33100dbcf83cf7231f9ecc80fe1805c167e8887
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-08 14:56:23 +0200

    fix inv

--- components/inventar.php
+++ components/inventar.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	include "../phplogic/vlastnictvi.php";
+	include "../vlastnictvi.php";
 	include "updatevyrob.php";
 	echo '<div class="panel panel-default"><div class="panel-heading"><h2 class="panel-title">Inventář:</h2></div><div class="panel-body"><ul class="list-group">';//<table style="background-color: #fff" class="table table-bordered table-responsive table-hover"><tr><td>Peniaze</td><td>'.$vlastnictvi[0].'</td></tr>';
 	$dotaz = 'SELECT * FROM veci';
