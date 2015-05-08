f8dcb4b18d6deb5b55583b1eaaf7f2389268b28a
Jiří Vozár <jiri.vozar@gmail.com>
2015-05-08 10:11:21 +0200

    Fix nadpisů u kuponů.

--- admin/kupony.php
+++ admin/kupony.php
@@ -6,7 +6,7 @@ include "admin/tvorbakuponu.php";
 <div class="col-xs-12">
 	<div class="panel panel-default">
 		<div class="panel-heading">
-			<h1 class="panel-title">Seznam kuponů/poukázek</h1>
+			<h1 class="panel-title">Tvorba kuponů/poukázek</h1>
 		</div>
 		<div class="panel-body" style="width: 100%; heigth: 100%; text-align:left;">
 			<form action="admin.php" method="GET">
@@ -15,7 +15,7 @@ include "admin/tvorbakuponu.php";
 				$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 				while ($zaznam = mysql_fetch_array($vysledek))
 				{
-					//TODO: hrml/css guru: nějak hezky to pozarovnávat (víc na 1 řádek) | pokus 1 probehl
+					//TODO: hrml/css guru: nějak hezky to pozarovnávat (víc na 1 řádek) | pokus 1 probehl | zatim super (jirvoz)
 					echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2"><div class="form-group"><label for="'.$zaznam['idveci'].'">'.$zaznam['nazev'].'</label>';
 					echo '<input type="number" name="'.$zaznam['idveci'].'" id="'.$zaznam['idveci'].'" min="0" max="10000" value="0" class="form-control" placeholder="'.$zaznam['nazev'].'">';
 					echo '</div></div>';
@@ -31,7 +31,7 @@ include "admin/tvorbakuponu.php";
 <div class="col-xs-12">
 	<div class="panel panel-default">
 		<div class="panel-heading">
-			<h1 class="panel-title">Tvorba kuponů/poukázek</h1>
+			<h1 class="panel-title">Seznam kuponů/poukázek</h1>
 		</div>
 		<div class="panel-body" style="width: 100%; heigth: 100%; text-align:left;">
 			<?php
