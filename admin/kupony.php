81f8110143e78a04449072a1015306ed9b12beae
Jiří Vozár <jiri.vozar@gmail.com>
2015-05-08 20:08:11 +0200

    Fix includu v adminovi.

--- admin/kupony.php
+++ admin/kupony.php
@@ -15,7 +15,7 @@ include "admin/tvorbakuponu.php";
 				$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 				while ($zaznam = mysql_fetch_array($vysledek))
 				{
-					//TODO: hrml/css guru: nějak hezky to pozarovnávat (víc na 1 řádek) | pokus 1 probehl | zatim super (jirvoz)
+					//TODO: html/css guru: nějak hezky to pozarovnávat (víc na 1 řádek) | pokus 1 probehl | zatim super (jirvoz)
 					echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2"><div class="form-group"><label for="'.$zaznam['idveci'].'"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img> '.$zaznam['nazev'].'</label>';
 					echo '<input type="number" name="'.$zaznam['idveci'].'" id="'.$zaznam['idveci'].'" min="0" max="10000" value="0" class="form-control" placeholder="'.$zaznam['nazev'].'">';
 					echo '</div></div>';
@@ -27,7 +27,6 @@ include "admin/tvorbakuponu.php";
 	</div>
 </div>
 <!-- seznam kuponů -->
-<!-- TODO: hezky graficky upravit (html/css guru) -->
 <div class="col-xs-12">
 	<div class="panel panel-default">
 		<div class="panel-heading">
@@ -35,7 +34,7 @@ include "admin/tvorbakuponu.php";
 		</div>
 		<div class="panel-body" style="width: 100%; heigth: 100%; text-align:left;">
 			<?php
-			include "../vlastnictvi.php";
+			include "vlastnictvi.php";
 			$dotaz = 'SELECT * FROM kupony';
 			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 			while ($zaznam = mysql_fetch_array($vysledek))
