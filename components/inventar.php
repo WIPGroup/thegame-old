8372682b56276dc738376505785eb8f44173c81a
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-08 23:29:20 +0200

    inventory collapse

--- components/inventar.php
+++ components/inventar.php
@@ -1,13 +1,21 @@
 <?php
-	session_start();
-	include "../vlastnictvi.php";
-	include "updatevyrob.php";
-	echo '<div class="panel panel-primary"><div class="panel-heading"><h2 class="panel-title">Inventář:</h2></div><ul class="list-group" style="text-align: left">';//<table style="background-color: #fff" class="table table-bordered table-responsive table-hover"><tr><td>Peniaze</td><td>'.$vlastnictvi[0].'</td></tr>';
-	$dotaz = 'SELECT * FROM veci';
-	$vysledek = mysql_query($dotaz) or die(mysql_error($db));
-	while ($zaznam = mysql_fetch_array($vysledek)) {
-		echo '<li class="list-group-item"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> '.$zaznam['nazev'].'</li>';
-		//'<button class="btn btn-link col-lg-4" title="'.$zaznam['nazev'].'"><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> <img id="item" src="icons/'.$zaznam['nazev'].'.png"></img></button>';
-	}
-	echo '</ul></div>';
+session_start();
+include "../vlastnictvi.php";
+include "updatevyrob.php";
 ?>
+<div class="panel panel-primary">
+	<div class="panel-heading" data-toggle="collapse" href="#inv" style="cursor: pointer">
+		<h1 class="panel-title">Inventář</h1>
+	</div>
+	<div id="inv" class="panel-collapse collapse in" style="text-align: left">
+		<ul class="list-group" style="text-align: left">
+			<?php
+			$dotaz = 'SELECT * FROM veci';
+			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
+			while ($zaznam = mysql_fetch_array($vysledek)) {
+				echo '<li class="list-group-item"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> '.$zaznam['nazev'].'</li>';
+			}
+			?>
+		</ul>
+	</div>
+</div>
