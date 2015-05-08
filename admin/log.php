10d2d1c044fc965d4316e46d755283b8f5534c56
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-08 21:05:37 +0200

    fix panel collapsing

--- admin/log.php
+++ admin/log.php
@@ -4,19 +4,17 @@
 			<h1 class="panel-title">Log</h1>
 		</div>
 		<div id="log" class="panel-collapse collapse">
-			<div class="panel-body" style="width: 100%; heigth: 100%; text-align:left;">
-				<ul class="list-group">
-					<?php
-					$dotaz = 'SELECT * FROM log, hraci WHERE hrac=idhrace ORDER BY cas DESC LIMIT 100';
-					$vysledek = mysql_query($dotaz) or die(mysql_error($db));
-					while ($zaznam = mysql_fetch_array($vysledek))
-					{
-						echo '<li class="list-group-item">['.date("Y.m.d H:i:s",$zaznam['cas'])."][".$zaznam['jmeno']."]: ".$zaznam['text'];
-						echo "</li>";
-					}
-					?>
-				</ul>
-			</div>
+			<ul class="list-group">
+				<?php
+				$dotaz = 'SELECT * FROM log, hraci WHERE hrac=idhrace ORDER BY cas DESC LIMIT 100';
+				$vysledek = mysql_query($dotaz) or die(mysql_error($db));
+				while ($zaznam = mysql_fetch_array($vysledek))
+				{
+					echo '<li class="list-group-item">['.date("Y.m.d H:i:s",$zaznam['cas'])."][".$zaznam['jmeno']."]: ".$zaznam['text'];
+					echo "</li>";
+				}
+				?>
+			</ul>
 		</div>
 	</div>
 </div>
