c5e08f087e884fe76bf50484371e81f23d54c304
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-08 20:35:35 +0200

    make log collapsible

--- admin/log.php
+++ admin/log.php
@@ -1,20 +1,22 @@
 <div class="col-xs-12">
 	<div class="panel panel-default">
-		<div class="panel-heading">
+		<div class="panel-heading" data-toggle="collapse" href="#log">
 			<h1 class="panel-title">Log</h1>
 		</div>
-		<div class="panel-body" style="width: 100%; heigth: 100%; text-align:left;">
-			<ul class="list-group">
-				<?php
-				$dotaz = 'SELECT * FROM log, hraci WHERE hrac=idhrace ORDER BY cas DESC LIMIT 100';
-				$vysledek = mysql_query($dotaz) or die(mysql_error($db));
-				while ($zaznam = mysql_fetch_array($vysledek))
-				{
-					echo '<li class="list-group-item">['.date("Y.m.d H:i:s",$zaznam['cas'])."][".$zaznam['jmeno']."]: ".$zaznam['text'];
-					echo "</li>";
-				}
-				?>
-			</ul>
+		<div id="log" class="panel-collapse collapse">
+			<div class="panel-body" style="width: 100%; heigth: 100%; text-align:left;">
+				<ul class="list-group">
+					<?php
+					$dotaz = 'SELECT * FROM log, hraci WHERE hrac=idhrace ORDER BY cas DESC LIMIT 100';
+					$vysledek = mysql_query($dotaz) or die(mysql_error($db));
+					while ($zaznam = mysql_fetch_array($vysledek))
+					{
+						echo '<li class="list-group-item">['.date("Y.m.d H:i:s",$zaznam['cas'])."][".$zaznam['jmeno']."]: ".$zaznam['text'];
+						echo "</li>";
+					}
+					?>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
