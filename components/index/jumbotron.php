0dd5762e369a853a4363cc460e66ccc378c76a65
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-09 14:29:01 +0200

    edit jumbotron

--- components/index/jumbotron.php
+++ components/index/jumbotron.php
@@ -1,7 +1,12 @@
-<div class="jumbotron" style="text-align: left">
-	<h1>Hleda se pozadi</h1>
-	<p>Hleda se vhodne pozadi. Takhle, vzhledem k tomu ze je to hra, musí to mít pozitivní background, nesmí odvádět pozornost, tj žádná fotka a aby se to hodilo k designu tak nějaký cartoon style. Všechno je laděné do modra, tj barvičky se musí vynahradit background ta je tam nějaká obrovská duha :D</p>
-	<p>
-		<a class="btn btn-primary btn-lg" href="https://www.google.cz/search?q=background&espv=2&biw=1920&bih=971&site=webhp&tbm=isch&source=lnt&tbs=isz:ex,iszw:1280,iszh:1024">Vyhledat</a>
-	</p>
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h1 class="panel-title">Oznameni</h1>
+	</div>
+	<div class="panel-body jumbotron" style="margin-bottom: 0px">
+		<h1>Hleda se pozadi</h1>
+		<p>Hleda se vhodne pozadi. Takhle, vzhledem k tomu ze je to hra, musí to mít pozitivní background, nesmí odvádět pozornost, tj žádná fotka a aby se to hodilo k designu tak nějaký cartoon style. Všechno je laděné do modra, tj barvičky se musí vynahradit background ta je tam nějaká obrovská duha :D</p>
+		<p>
+			<a class="btn btn-primary btn-lg" href="https://www.google.cz/search?q=background&amp;espv=2&amp;biw=1920&amp;bih=971&amp;site=webhp&amp;tbm=isch&amp;source=lnt&amp;tbs=isz:ex,iszw:1280,iszh:1024">Vyhledat</a>
+		</p>
+	</div>
 </div>
