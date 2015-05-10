3d851051f5c88bb80dedde71301fb41ee2e9bd9f
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-10 10:57:27 +0200

    fix profile links

--- profile/index.php
+++ profile/index.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-require "components/head.php";
-require "dblogin.php";
-require "login.php";
-include "components/navbar.php";
+require "../components/head.php";
+require "../dblogin.php";
+require "../login.php";
+include "../components/navbar.php";
 if ($prihlasen){
 	echo "Trade history setting a tak";
 } else {
-	include "components/form.php";
+	include "../components/form.php";
 }
 ?>
 </body>
