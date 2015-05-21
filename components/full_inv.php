457a27b446ed416563f0d60324fea56df12b5069
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-21 17:20:39 +0200

    add TODO

---
+++ components/full_inv.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include "../vlastnictvi.php";
+include "updatevyrob.php";
+?>
+<div class="panel panel-primary">
+	<div class="panel-heading" data-toggle="collapse" href="#inv" style="cursor: pointer">
+		<h1 class="panel-title">Inventář</h1>
+	</div>
+	<div id="inv" class="panel-collapse collapse in" style="text-align: left">
+		<ul id="items" class="list-group" style="text-align: left">
+			<?php
+			$dotaz = 'SELECT * FROM veci';
+			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
+			while ($zaznam = mysql_fetch_array($vysledek)) {
+				//if ($vlastnictvi[$zaznam['idveci']] > 0)
+					echo '<li class="list-group-item"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> '.$zaznam['nazev'].'</li>';
+			}
+			?>
+		</ul>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include "components/navbar.php";
 if ($prihlasen)
 {
 	include 'components/index/jumbotron.php';
-	echo '<div id="inventar" class="col-xs-3"></div>';
+	echo '<div id="inventar" class="col-xs-3"></div>'; //TODO: full_inv.php
 	include "components/kupony.php";
 }
 else
