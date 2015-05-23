1562bdb310d1f37c3b5a93decae99e8e33edbb7f
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-23 22:15:50 +0200

    stavba kompu bootstrap

--- components/stavba.php
+++ components/stavba.php
@@ -3,7 +3,7 @@ include "components/sestavit.php";
 //TODO Napsat @j2ghz az to bude pripravene na graficke vylepseni
 echo "<h1>stavba počítačů</h1>";
 
-echo '<form action="build.php" method="GET">';
+echo '<form class="form-inline" action="build.php" method="GET">';
 
 $dotaz = 'SELECT * FROM veci WHERE typ<>""';
 $vysledek = mysql_query($dotaz) or die(mysql_error($db));
@@ -25,15 +25,15 @@ while ($zaznam = mysql_fetch_array($vysledek))
 			$hdds .= '<option value="'.$zaznam['idveci'].'">'.$zaznam['nazev'].' ('.$vlastnictvi[$zaznam['idveci']].')</option>'."\n";
 	}
 }
-echo '<select name="mb" id="mb">'.$mbs.'</select>';
-echo '<select name="cpu" id="cpu">'.$cpus.'</select>';
-echo '<select name="gpu" id="gpu">'.$gpus.'</select>';
-echo '<select name="ram1" id="ram1">'.$rams.'</select>';
-echo '<select name="ram2" id="ram2">'.$rams.'</select>';
-echo '<select name="ram3" id="ram3">'.$rams.'</select>';
-echo '<select name="ram4" id="ram4">'.$rams.'</select>';
-echo '<select name="psu" id="psu">'.$psus.'</select>';
-echo '<select name="hdd" id="hdd">'.$hdds.'</select>';
-echo '<button type="submit">Sestavit</button>';
+echo '<select class="form-control" name="mb" id="mb">'.$mbs.'</select>';
+echo '<select class="form-control" name="cpu" id="cpu">'.$cpus.'</select>';
+echo '<select class="form-control" name="gpu" id="gpu">'.$gpus.'</select>';
+echo '<select class="form-control" name="ram1" id="ram1">'.$rams.'</select>';
+echo '<select class="form-control" name="ram2" id="ram2">'.$rams.'</select>';
+echo '<select class="form-control" name="ram3" id="ram3">'.$rams.'</select>';
+echo '<select class="form-control" name="ram4" id="ram4" disabled>'.$rams.'</select>'; //TODO priklad zakazaneho vyberu: dat k prazdnym vyberum a k tlacitku, kdyz neni zvoleny funkcni komp
+echo '<select class="form-control" name="psu" id="psu">'.$psus.'</select>';
+echo '<select class="form-control" name="hdd" id="hdd">'.$hdds.'</select>';
+echo '<button class="btn btn-primary" type="submit">Sestavit</button>';
 echo '</form>';
 ?>
