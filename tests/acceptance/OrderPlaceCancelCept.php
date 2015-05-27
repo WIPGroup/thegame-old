e0174d9f488a02df4df545ca75f567d9364d3f1d
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-27 17:17:24 +0200

    add test for order placing and cancelling

---
+++ tests/acceptance/OrderPlaceCancelCept.php
@@ -0,0 +1,20 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Place and cancel an order');
+$I->amGoingTo('Login');
+$I->amOnPage('/');
+$I->fillField('hrac','root');
+$I->fillField('heslo','root');
+$I->click('Prihlásiť');
+$I->click('Trh');
+$I->amGoingTo('Place an order for 1 Gold in exchange for 1 Money');
+$I->selectOption('form select[name=nabizi]', 'Money');
+$I->fillField('mnoznabizi','2');
+$I->selectOption('form select[name=chce]', 'Gold');
+$I->fillField('mnozchce','3');
+$I->click('Odoslať');
+$I->amGoingTo('Switch to Moje nabídky tab');
+$I->click('Moje nabídky');
+//TODO see the order
+$I->amGoingTo('Cancel the order');
+//TODO cancel the order
