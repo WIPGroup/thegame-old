18e96cdcd0c91fe1b127f3b733039e6d6ab2f1c7
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-27 18:43:36 +0200

    add crafting test

---
+++ tests/acceptance/CraftFirstThingCept.php
@@ -0,0 +1,12 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Craft a RAM');
+$I->amGoingTo('Login');
+$I->amOnPage('/');
+$I->fillField('hrac','root');
+$I->fillField('heslo','root');
+$I->click('Prihlásiť');
+$I->amGoingTo('Place crafting order');
+$I->click('Výroba');
+$I->click('Vyrobit');
+$I->see('RAM hotovo');
