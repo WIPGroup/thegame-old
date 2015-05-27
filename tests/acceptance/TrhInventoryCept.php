6b0b8c6d1f812ebe2d2ec07bb5f722a71d5f4fa3
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-27 17:17:11 +0200

    inventory presence on trh better description

--- tests/acceptance/TrhInventoryCept.php
+++ tests/acceptance/TrhInventoryCept.php
@@ -1,10 +1,11 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('Check inventory for money, iron and gold');
+$I->wantTo('Login');
 $I->amOnPage('/');
 $I->fillField('hrac','root');
 $I->fillField('heslo','root');
 $I->click('Prihlásiť');
+$I->wantTo('Check inventory for money, iron and gold');
 $I->click('Trh');
 $I->see('Money');
 $I->see('Gold');
