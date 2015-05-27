6623897fb1501e58093afde7ccac4bb864985baa
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-27 19:01:57 +0200

    build layout

--- components/stavba.php
+++ components/stavba.php
@@ -1,16 +1,23 @@
 <?php
 include "components/sestavit.php";
 echo "<h1>stavba počítačů</h1>";
-echo '<form class="form-inline" action="build.php" method="GET" id="build">';
-
-$mbs = ""; $cpus = ""; $gpus = ""; $rams = ""; $psus = ""; $hdds = ""; 
+?>
+<div class="col-md-6 col-xs-12">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h1 class="panel-title">Sestavit</h1>
+		</div>
+		<div class="panel-body">
+			<form class="form-inline" action="build.php" method="GET" id="build">
+<?php
+$mbs = ""; $cpus = ""; $gpus = ""; $rams = ""; $psus = ""; $hdds = "";
 $veci = null;
 $dotaz = 'SELECT * FROM veci';// WHERE typ<>""';
 $vysledek = mysql_query($dotaz) or die(mysql_error($db));
 while ($zaznam = mysql_fetch_array($vysledek))
 {
 	$veci[$zaznam['idveci']] = $zaznam['nazev'];
-	
+
 	if ($vlastnictvi[$zaznam['idveci']] > 0)
 	{
 		if ($zaznam['typ'] == "mb")
@@ -38,7 +45,17 @@ echo '<select class="form-control" name="psu" id="psu">'.$psus.'</select>';
 echo '<select class="form-control" name="hdd" id="hdd">'.$hdds.'</select>';
 echo '<button class="btn btn-primary" type="submit">Sestavit</button>';
 echo '</form>';
-
-echo '<h2>Moje sestavy</h2>';
-echo '<div id="sestavy"></div>';
 ?>
+		</div>
+	</div>
+</div>
+<div class="col-md-6 col-xs-12">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h1 class="panel-title">Moje sestavy</h1>
+		</div>
+	</div>
+	<div class="panel-body">
+		<div id="sestavy"></div>
+	</div>
+</div>
