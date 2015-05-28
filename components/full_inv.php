6baf2c396ab56037fbf8050aa734b8d834cc08f0
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-28 13:29:53 +0200

    enable bootstrap on buttons

--- components/full_inv.php
+++ components/full_inv.php
@@ -8,13 +8,13 @@ include "updatevyrob.php";
 		<h1 class="panel-title">Inventář</h1>
 	</div>
 	<div id="inv" class="panel-collapse collapse in" style="text-align: left">
-		<div class="button-group filter-button-group">
-			<button data-filter="*">show all</button>
-			<button data-filter=".money">Money</button>
-			<button data-filter=".transition">transition</button>
-			<button data-filter=".alkali, .alkaline-earth">alkali & alkaline-earth</button>
-			<button data-filter=":not(.transition)">not transition</button>
-			<button data-filter=".metal:not(.transition)">metal but not transition</button>
+		<div class="btn-group button-group filter-button-group">
+			<button class="btn" data-filter="*">show all</button>
+			<button class="btn" data-filter=".money">Money</button>
+			<button class="btn" data-filter=".transition">transition</button>
+			<button class="btn" data-filter=".alkali, .alkaline-earth">alkali & alkaline-earth</button>
+			<button class="btn" data-filter=":not(.transition)">not transition</button>
+			<button class="btn" data-filter=".metal:not(.transition)">metal but not transition</button>
 		</div>
 		<div class="grid js-isotope" data-isotope-options='{ "itemSelector": ".item", "layoutMode": "packery" }'>
 			<?php
@@ -22,7 +22,7 @@ include "updatevyrob.php";
 			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 			while ($zaznam = mysql_fetch_array($vysledek)) {
 				//if ($vlastnictvi[$zaznam['idveci']] > 0)
-				echo '<div class="grid-item '.$zaznam['nazev'].'"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> '.$zaznam['nazev'].'</div>';
+				echo '<div class="grid-item '.$zaznam['nazev'].'"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> '.$zaznam['nazev'].'</div>'; //TODO do classy pridat ruzne veci podle kterych se to da tridit a filtrovat
 			}
 		?>
 		</div>
