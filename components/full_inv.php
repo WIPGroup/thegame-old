d8b58f4e58874cb6ff7697eb9cbae73e687e2551
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-28 13:45:19 +0200

    filtering proposal

--- components/full_inv.php
+++ components/full_inv.php
@@ -9,11 +9,10 @@ include "updatevyrob.php";
 	</div>
 	<div id="inv" class="panel-collapse collapse in" style="text-align: left">
 		<div class="btn-group button-group filter-button-group">
-			<button class="btn" data-filter="*">show all</button>
+			<button class="btn" data-filter="*">Show all</button>
 			<button class="btn" data-filter=".money">Money</button>
-			<button class="btn" data-filter=".transition">transition</button>
-			<button class="btn" data-filter=".alkali, .alkaline-earth">alkali & alkaline-earth</button>
-			<button class="btn" data-filter=":not(.transition)">not transition</button>
+			<button class="btn" data-filter=".gold, .iron, .silicon">Gold, Iron & Silicon</button>
+			<button class="btn" data-filter=":not(.money)">No Money</button>
 			<button class="btn" data-filter=".metal:not(.transition)">metal but not transition</button>
 		</div>
 		<div class="grid js-isotope" data-isotope-options='{ "itemSelector": ".item", "layoutMode": "packery" }'>
@@ -22,9 +21,19 @@ include "updatevyrob.php";
 			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 			while ($zaznam = mysql_fetch_array($vysledek)) {
 				//if ($vlastnictvi[$zaznam['idveci']] > 0)
-				echo '<div class="grid-item '.$zaznam['nazev'].'"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> '.$zaznam['nazev'].'</div>'; //TODO do classy pridat ruzne veci podle kterych se to da tridit a filtrovat
+				echo '<div class="grid-item '.$zaznam['nazev'].'">';
+				echo '<img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img>';
+				echo '<span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> ';
+				echo $zaznam['nazev'].'</div>';
+				//TODO do classy pridat ruzne veci podle kterych se to da tridit a filtrovat
 			}
 		?>
 		</div>
 	</div>
 </div>
+<script>
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $container.isotope({ filter: filterValue });
+});
+</script>
