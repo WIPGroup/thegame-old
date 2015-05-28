4472b8414b531375098d745a6c44ae4f662d51c6
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-28 09:40:38 +0200

    add filters

--- components/full_inv.php
+++ components/full_inv.php
@@ -8,15 +8,23 @@ include "updatevyrob.php";
 		<h1 class="panel-title">Inventář</h1>
 	</div>
 	<div id="inv" class="panel-collapse collapse in" style="text-align: left">
+		<div class="button-group filter-button-group">
+			<button data-filter="*">show all</button>
+			<button data-filter=".money">Money</button>
+			<button data-filter=".transition">transition</button>
+			<button data-filter=".alkali, .alkaline-earth">alkali & alkaline-earth</button>
+			<button data-filter=":not(.transition)">not transition</button>
+			<button data-filter=".metal:not(.transition)">metal but not transition</button>
+		</div>
 		<div class="grid js-isotope" data-isotope-options='{ "itemSelector": ".item", "layoutMode": "packery" }'>
 			<?php
 			$dotaz = 'SELECT * FROM veci';
 			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 			while ($zaznam = mysql_fetch_array($vysledek)) {
 				//if ($vlastnictvi[$zaznam['idveci']] > 0)
-					echo '<div class="grid-item"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> '.$zaznam['nazev'].'</div>';
+				echo '<div class="grid-item '.$zaznam['nazev'].'"><img id="item-sm" src="icons/'.$zaznam['nazev'].'.png"></img><span class="badge">'.$vlastnictvi[$zaznam['idveci']].'</span> '.$zaznam['nazev'].'</div>';
 			}
-			?>
+		?>
 		</div>
 	</div>
 </div>
