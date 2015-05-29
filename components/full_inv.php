8c008a5916cea1a3d534610ff3f40d56e6e81028
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-29 21:24:39 +0000

    add TODO

--- components/full_inv.php
+++ components/full_inv.php
@@ -36,7 +36,7 @@ include "updatevyrob.php";
 			$dotaz = 'SELECT * FROM veci';
 			$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 			while ($zaznam = mysql_fetch_array($vysledek)) { //vymyslet http://isotope.metafizzy.co/filtering.html
-				echo '<div data-tier="T0" data-type="'.$zaznam['typ'].'" class="grid-item '.$zaznam['typ'].'" style="background-image: url(\'icons/'.$zaznam['nazev'].'.png\'); background-size: 128px 128px;">';
+				echo '<div data-tier="T0" data-type="'.$zaznam['typ'].'" class="grid-item '.$zaznam['typ'].'" style="background-image: url(\'icons/'.$zaznam['nazev'].'.png\'); background-size: 128px 128px;">'; //TODO pokud hrac item nema, pridat do style "opacity: 0.4;"
 				echo '<span class="badge count">'.$vlastnictvi[$zaznam['idveci']].'</span>';
 				echo '<span class="badge power">'.$zaznam['vykon'].'</span>';
 				echo '<span class="label label-default name">'.$zaznam['nazev'].'</span>';
