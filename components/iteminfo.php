a8ba1e94a308b74cc9db48b2b9f38554f480421f
opuro <vaclav.janc@gmail.com>
2015-06-03 19:32:25 +0200

    Kdo vymyslel v PHP dolary pro proměnné nechť trápí se.

--- components/iteminfo.php
+++ components/iteminfo.php
@@ -5,5 +5,5 @@ if (isset($_GET['item'])){
   $dotaz = 'SELECT * FROM veci WHERE idveci='.$_GET['item'];
   $vysledek = mysql_query($dotaz) or die(mysql_error($db));
   $zaznam = mysql_fetch_array($vysledek);
-  echo zaznam['nazev'];
+  echo $zaznam['nazev'];
 }
