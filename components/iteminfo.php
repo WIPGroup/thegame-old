f302f2c6ee5b81c42ce8f5a8ec3e39e41ea3a71c
opuro <vaclav.janc@gmail.com>
2015-06-04 19:00:00 +0200

    Remove redundant file

--- components/iteminfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include "../vlastnictvi.php";
-if (isset($_GET['item'])){
-  $dotaz = 'SELECT * FROM veci WHERE idveci='.$_GET['item'];
-  $vysledek = mysql_query($dotaz) or die(mysql_error($db));
-  $zaznam = mysql_fetch_array($vysledek);
-  echo $zaznam['nazev'];
-}
