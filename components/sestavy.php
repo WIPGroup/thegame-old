6cf1c5c63e724569cd0ce67cbd93d24fe0ecacd1
Jozef Hollý <j2.00ghz@gmail.com>
2015-06-10 16:45:31 +0200

    uprava zobrazeni sestav

--- components/sestavy.php
+++ components/sestavy.php
@@ -11,18 +11,21 @@ while ($zaznam = mysql_fetch_array($vysledek))
 $pocveci = count($veci);
 $dotaz = 'SELECT * FROM sestavy WHERE hrac='.$_SESSION['hrac'];
 $vysledek = mysql_query($dotaz) or die(mysql_error($db));
+echo '<nav>';
 while ($zaznam = mysql_fetch_array($vysledek))
 {
+	echo '<ul class="pagination">';
 	$obsah = explode(';', $zaznam['obsah']);
 	for ($i = 0; $i < $pocveci; $i++)
 		if ($obsah[$i] > 0)
 		{
+			echo '<li>';
 			if ($obsah[$i] > 1)
 				echo '<span class="badge">'.$obsah[$i].'</span>';
-			echo $veci[$i].'	';
-			echo ', ';
+			echo $veci[$i].'</li>';
 		}
-	echo 'Výkon: '.$zaznam['vykon'].'<br>';
-	echo 'přepnutí body/výzkum; tlačítko rozebrat<br>';
+	echo '<li>Výkon: '.$zaznam['vykon'].'</li>';
+	echo '<li>přepnutí body/výzkum; tlačítko rozebrat</li></ul>';
 }
+echo '</nav>';
 ?>
