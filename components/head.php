16ea188df046b6e9982e0482e2000e80b1c45300
Jozef Hollý <j2.00ghz@gmail.com>
2015-06-28 14:15:19 +0200

    make scripts async

--- components/head.php
+++ components/head.php
@@ -6,44 +6,44 @@
 	<title>TheGame</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<!-- jQuery -->
-	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script async src="bower_components/jquery/dist/jquery.min.js"></script>
 	<!-- Rollbar JS -->
-	<script src="js/rollbar.js"></script>
+	<script async src="js/rollbar.js"></script>
 	<!-- Pace -->
 	<link href="bower_components/pace/themes/green/pace-theme-minimal.css" rel="stylesheet" />
 	<script src="bower_components/pace/pace.js"></script>
 	<!-- Offline -->
 	<link href="bower_components/offline/themes/offline-theme-slide.css" rel="stylesheet" />
 	<link href="bower_components/offline/themes/offline-language-english.css" rel="stylesheet" />
-	<script src="bower_components/offline/offline.min.js"></script>
+	<script async src="bower_components/offline/offline.min.js"></script>
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
 	<!--<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css"> bootstrap original theme-->
 	<!-- Bootstrap spacelab theme -->
 	<link rel="stylesheet" href="bower_components/bootswatch-dist/css/bootstrap.min.css">
-	<script src="bower_components/bootswatch-dist/js/bootstrap.min.js"></script>
+	<script async src="bower_components/bootswatch-dist/js/bootstrap.min.js"></script>
 	<!-- DataTables -->
 	<link rel="stylesheet" type="text/css" href="bower_components/datatables/media/css/jquery.dataTables.min.css">
-	<script type="text/javascript" charset="utf8" src="bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+	<script async type="text/javascript" charset="utf8" src="bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
 	<!-- SweetAlert -->
-	<script src="bower_components/sweetalert/dist/sweetalert.min.js"></script>
+	<script async src="bower_components/sweetalert/dist/sweetalert.min.js"></script>
 	<link rel="stylesheet" href="bower_components/sweetalert/dist/sweetalert.css">
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="bower_components/animate.css/animate.min.css">
+	<!-- Animate.css Zatim neni potreba FIX TODO
+	<link rel="stylesheet" href="bower_components/animate.css/animate.min.css">-->
 	<!-- The noUiSlider script and stylesheet -->
 	<link href="bower_components/nouislider/distribute/jquery.nouislider.min.css" rel="stylesheet">
-	<script src="bower_components/nouislider/distribute/jquery.nouislider.all.min.js"></script>
+	<script async src="bower_components/nouislider/distribute/jquery.nouislider.all.min.js"></script>
 	<!-- bootstrap-select -->
 	<link href="bower_components/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
-	<script src="bower_components/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+	<script async src="bower_components/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
 	<!-- Isotope -->
-	<script src="bower_components\isotope\dist\isotope.pkgd.js"></script>
-	<script src="bower_components\isotope-packery\packery-mode.pkgd.min.js"></script>
+	<script async src="bower_components\isotope\dist\isotope.pkgd.js"></script>
+	<script async src="bower_components\isotope-packery\packery-mode.pkgd.min.js"></script>
 	<!-- Nase veci -->
 	<link rel="stylesheet" href="main.css">
 	<link rel="stylesheet" href="inventory.css">
 	<link rel="shortcut icon" href="favicon.ico">
-	<script src="js/scripts.js"></script>
+	<script async src="js/scripts.js"></script>
 </head>
 <body>
 	<?php //Crash monitoring
