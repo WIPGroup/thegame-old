c8d93f1bef14a060a5d1efd573150b127081d560
Jozef Hollý <j2.00ghz@gmail.com>
2015-06-28 14:01:50 +0200

    fix width of jumbotron

--- components/index/jumbotron.php
+++ components/index/jumbotron.php
@@ -1,9 +1,11 @@
-<div class="panel panel-warning animated pulse">
-	<div class="panel-heading" data-toggle="collapse" href="#jumbo" style="cursor: pointer">
-		<h1 class="panel-title">Oznámení</h1>
-	</div>
-	<div id="jumbo" class="panel-body jumbotron panel-collapse collapse" style="margin-bottom: 0px">
-		<h1>Informacni tabule</h1>
-		<p>zatim nic<p>
+<div class="col-xs-12">
+	<div class="panel panel-warning animated pulse">
+		<div class="panel-heading" data-toggle="collapse" href="#jumbo" style="cursor: pointer">
+			<h1 class="panel-title">Oznámení</h1>
+		</div>
+		<div id="jumbo" class="panel-body jumbotron panel-collapse collapse" style="margin-bottom: 0px">
+			<h1>MOBILE FRIENDLY</h1>
+			<p>konecne<p>
 		</div>
 	</div>
+</div>
