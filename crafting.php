b76919e5f550b9daedaed97e1f134c7c26491e6a
Jozef Hollý <j2.00ghz@gmail.com>
2015-07-03 20:28:26 +0200

    rozmisteni poslednich casti

--- crafting.php
+++ crafting.php
@@ -36,11 +36,11 @@ if ($prihlasen)
 		for ($i = 0; $i < $pocsurovin; $i++)
 		{
 			if ($suroviny[$i] > 0)
-			echo '<div class="craft-suroviny"><span class="badge">'.$i.'</span><img src="icons/'.$i.'.png"></img><span class="label label-default">'.$veci[$i].'</span></div><br>';
+			echo '<div class="craft-suroviny"><span class="badge">'.$i.'</span><img src="icons/'.$i.'.png"></img><span class="label label-default">'.$veci[$i].'</span></div>';
 		}
 
 		echo '<div class="craft-time">'.$zaznam['doba'].' s</div>';
-		echo '<div class="craft-vyzkum">'.$zaznam['nazev'].'</div>';
+		echo '<div class="craft-vyzkum label label-warning">'.$zaznam['nazev'].'</div>';
 
 		echo '<div class="craft-vyrob"><input type="number" name="pocet" data-idreceptu="'.$zaznam['idreceptu'].'" value="1" min="1" max="1000"><br>';
 
