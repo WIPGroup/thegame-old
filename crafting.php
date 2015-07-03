7f9e63fae46bf69e0a104ea650a538a23ea76842
Jozef Hollý <j2.00ghz@gmail.com>
2015-07-03 20:48:56 +0200

    remove double time

--- crafting.php
+++ crafting.php
@@ -35,11 +35,10 @@ if ($prihlasen)
 		$pocsurovin = count($suroviny);
 		for ($i = 0; $i < $pocsurovin; $i++)
 		{
-			if ($suroviny[$i] > 0)
+			if ($suroviny[$i] > 0) //TODO html kdyz bude cas tak do tablu
 			echo '<div class="craft-suroviny"><span class="badge">'.$i.'</span><img src="icons/'.$i.'.png"></img><span class="label label-default">'.$veci[$i].'</span></div>';
 		}
 		echo '<div class="craft-suroviny"><span class="badge">'.$zaznam['doba'].'</span><span class="glyphicon glyphicon-time"></span><span class="label label-default">Sekundy</span></div>';
-		echo '<div class="craft-time">'.$zaznam['doba'].' s</div>';
 		echo '<div class="craft-vyzkum label label-warning">'.$zaznam['nazev'].'</div>';
 
 		echo '<div class="craft-vyrob"><input type="number" name="pocet" data-idreceptu="'.$zaznam['idreceptu'].'" value="1" min="1" max="1000">';
