6ac196bb71b847489530c40c2e453ae6f55473e3
Jiří Vozár <jiri.vozar@gmail.com>
2015-07-03 10:04:52 +0200

    Soubor s SQL dotazem pro přidání všech věcí.

---
+++ veci.sql
@@ -0,0 +1,33 @@
+INSERT INTO veci (nazev, typ, vykon, socket, sloty, popis) VALUES
+('Peníze', '', 0, 0, '', 'Základní prostředek pro směnu, počítač z ních neposkládáš.'),
+('Železo', '', 0, 0, '', 'Surovina s chemickou značkou Fe.'),
+('Zlato', '', 0, 0, '', 'Surovina s chemickou značkou Au.'),
+('Křemík', '', 0, 0, '', 'Surovina s chemickou značkou Si.'),
+('Northbridge', '', 0, 0, '', ''),
+('Southbridge', '', 0, 0, '', ''),
+('Flash Bios', '', 0, 0, '', ''),
+('RAM slots', '', 0, 0, '', ''),
+('PCI-e', '', 0, 0, '', ''),
+('Core', '', 0, 0, '', ''),
+('L3 Cache', '', 0, 0, '', ''),
+('Graphics', '', 0, 0, '', ''),
+('Controller', '', 0, 0, '', ''),
+('Graphics core', '', 0, 0, '', ''),
+('VRAM', '', 0, 0, '', ''),
+('PCB', '', 0, 0, '', ''),
+('MOSFET', '', 0, 0, '', ''),
+('Intel D975XBX', 'mb', 1, 1, '4;3;1', 'Třída Pentium');
+('Abit IP35', 'mb', 1, 2, '4;1;1', 'Třída Core');
+('Asus P5Q Deluxe', 'mb', 1, 2, '4;2;1', 'Třída Core');
+('Asus EVGA 132-CK-NF78-A1', 'mb', 1, 2, '4;3;1', 'Třída Core');
+('Asus P7P55D Deluxe', 'mb', 1, 3, '4;3;1', 'Třída Nehalem');
+('EVGA P55 Classified', 'mb', 1, 3, '4;4;1', 'Třída Nehalem');
+('EVGA X58 Classified 4x SLI', 'mb', 1, 3, '6;4;1', 'Třída Nehalem');
+('CPU-AMD', 'cpu', 2, 1, '', 'ne tak dobrý popis cpu od amd'),
+('CPU-Intel', 'cpu', 20000, 2, '', 'überepic popis cpu od intelu'),
+('GPU', 'gpu', 5, 0, '', 'popis gpu'),
+('HDD', 'hdd', 1, 0, '', 'popis hdd'),
+('PSU', 'psu', 10, 0, '', 'popis psu'),
+('RAM', 'ram', 1, 0, '', 'popis ram'),
+('SSD', 'hdd', 2, 0, '', 'popis ssd'),
+('MotherBoard', 'mb', 1, 2, '2;1;2', 'popis mb');
