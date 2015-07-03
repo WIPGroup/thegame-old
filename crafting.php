e1cfaea44e2061890c6b2a8b79c3a85c502a087b
Jozef Hollý <j2.00ghz@gmail.com>
2015-07-03 20:37:40 +0200

    rozmisteni poslednich casti

--- crafting.php
+++ crafting.php
@@ -38,11 +38,11 @@ if ($prihlasen)
 			if ($suroviny[$i] > 0)
 			echo '<div class="craft-suroviny"><span class="badge">'.$i.'</span><img src="icons/'.$i.'.png"></img><span class="label label-default">'.$veci[$i].'</span></div>';
 		}
-
+		echo '<div class="craft-suroviny"><span class="badge">'.$zaznam['doba'].'</span><span class="glyphicon glyphicon-time"></span><span class="label label-default">Sekundy</span></div>';
 		echo '<div class="craft-time">'.$zaznam['doba'].' s</div>';
 		echo '<div class="craft-vyzkum label label-warning">'.$zaznam['nazev'].'</div>';
 
-		echo '<div class="craft-vyrob"><input type="number" name="pocet" data-idreceptu="'.$zaznam['idreceptu'].'" value="1" min="1" max="1000"><br>';
+		echo '<div class="craft-vyrob"><input type="number" name="pocet" data-idreceptu="'.$zaznam['idreceptu'].'" value="1" min="1" max="1000">';
 
 		$splnuje = true;
 
