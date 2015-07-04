754f222d13b74d34ecb2e24b4bfb62abebdd3022
Jiří Vozár <jiri.vozar@gmail.com>
2015-07-04 12:00:21 +0200

    Body čekají na ajax.

---
+++ components/body.php
@@ -0,0 +1,16 @@
+<?php
+//TODO odkomentovat pro ajax
+//session_start();
+//include '../vlastnictvi.php';
+?>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h1 class="panel-title">Body</h1>
+  </div>
+  <div class="panel-body" style="width: 100%; heigth: 100%">
+    <?php
+    echo 'Výzkumové body: '.$hrac['vyzkum'].'<br><strong>Výherní body: '.$hrac['body'].'</strong>';
+    ?>
+  </div>
+</div>
+
