6efc14953664ba0ce16b937947fe2e5f6cf73a32
Jozef Hollý <j2.00ghz@gmail.com>
2015-07-04 14:11:29 +0200

    fix pred zacatkem prace na lepsim staveni kompu

--- components/stavba2.php
+++ components/stavba2.php
@@ -1,7 +1,7 @@
 <?php
 require 'vlastnictvi.php';
 ?>
-<div class="col-md-2 col-xs-12">
+<div class="col-xs-12">
 	<div class="panel panel-primary">
 		<div class="panel-heading">
 			<h1 class="panel-title">Sestavit</h1>
