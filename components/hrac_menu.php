0eee9042586ebcc6044eb4ce9248fee860a6363d
Jozef Hollý <j2.00ghz@gmail.com>
2015-07-08 12:51:45 +0200

    glyph icon test

--- components/hrac_menu.php
+++ components/hrac_menu.php
@@ -24,7 +24,7 @@
 	<li>
 		<form action="logout.php" class="navbar-form">
 			<div style="text-align: center">
-				<button type="submit" class="btn btn-warninig">Odhlásiť</button>
+				<button type="submit" class="btn btn-warninig"><span class="glyphicon glyphicon-off"></span>Odhlásiť</button>
 			</div>
 		</form>
 	</ul>
