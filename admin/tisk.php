59f9dd579793927454fb938d42c98bdfa3a2e811
Jiří Vozár <jiri.vozar@gmail.com>
2015-07-09 20:57:11 +0200

    Verze kuponů na tisk.

--- admin/tisk.php
+++ admin/tisk.php
@@ -2,14 +2,14 @@
 session_start();
 require '../dblogin.php';
 require '../login.php';
-echo "<html><head></head><bodyi style=\"font-family:'Monospace'\">";
+echo "<html><head></head><bodyi style=\"font-family:'Monospace'; font-size:24px\">";
 if ($prihlasen && $_SESSION['hrac'] == 1)
 {
 	$dotaz = 'SELECT * FROM kupony ORDER BY cas';
 	$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 	while ($zaznam = mysql_fetch_array($vysledek))
 	{
-		echo strtoupper($zaznam['kod'])."&nbsp;";
+		echo strtoupper($zaznam['kod'])." ";
 	}
 
 }
