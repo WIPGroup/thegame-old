fb460d3ba495480e816c09b64bf06d7915511581
opuro <vaclav.janc@gmail.com>
2015-07-10 17:11:27 +0200

    RLY PHP, RLY

--- crafting.php
+++ crafting.php
@@ -33,7 +33,7 @@ if ($prihlasen)
 	while ($zaznam = mysql_fetch_array($vysledek))
 	{
 		//TODO: mobile  friendly
-		echo '<div class="grid-craft-item '.$typ[$zaznam['vyrobek']].'" style="background-image: url(icons/'.$zaznam['vyrobek'].'.png)" data-type="'.$typ[$zaznam['vyrobek']].'" data-idveci="'.$idveci[zaznam['vyrobek']].'">';
+		echo '<div class="grid-craft-item '.$typ[$zaznam['vyrobek']].'" style="background-image: url(icons/'.$zaznam['vyrobek'].'.png)" data-type="'.$typ[$zaznam['vyrobek']].'" data-idveci="'.$idveci[$zaznam['vyrobek']].'">';
 		echo '<span class="label label-default craft-name">'.$veci[$zaznam['vyrobek']].'</span>';
 
 		$suroviny = explode(';', $zaznam['suroviny']);
