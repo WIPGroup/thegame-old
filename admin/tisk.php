380b09718d4a93eedb21977a2d3b6c9fed093f2c
Jiří Vozár <jiri.vozar@gmail.com>
2015-07-10 18:37:58 +0200

    Velikost tisku.

--- admin/tisk.php
+++ admin/tisk.php
@@ -2,7 +2,7 @@
 session_start();
 require '../dblogin.php';
 require '../login.php';
-echo "<html><head></head><bodyi style=\"font-family:'DejaVu Sans Mono'; font-size:24px\">";
+echo "<html><head></head><bodyi style=\"font-family:'DejaVu Sans Mono'; font-size:32px\">";
 if ($prihlasen && $_SESSION['hrac'] == 1)
 {
 	$dotaz = 'SELECT * FROM kupony ORDER BY cas';
