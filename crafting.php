a93f5b3fc3115d4625bea242f04bd1598d42c106
opuro <vaclav.janc@gmail.com>
2015-07-10 18:53:41 +0200

    PHP je svině

--- crafting.php
+++ crafting.php
@@ -34,7 +34,10 @@ if ($prihlasen)
 	{
 		//TODO: mobile  friendly
 		echo '<div class="grid-craft-item '.$typ[$zaznam['vyrobek']].' t'.$zaznam['vyzkum'].' ';
-		if ($hrac['vyzkum'] < $zaznam['body']) echo 'yes' else echo 'no';
+		if ($hrac['vyzkum'] < $zaznam['body']) 
+			echo 'yes';
+		else
+			echo 'no';
 		echo '" style="background-image: url(icons/'.$zaznam['vyrobek'].'.png)" data-type="'.$typ[$zaznam['vyrobek']].'" data-idveci="'.$idveci[$zaznam['vyrobek']].'" data-tier="'.$zaznam['vyzkum'].'">';
 		echo '<span class="label label-default craft-name">'.$veci[$zaznam['vyrobek']].'</span>';
 
