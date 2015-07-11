f210a81b634d2d91cb129cd5e863a99436182540
Václav Jančařík <vaclav.janc@gmail.com>
2015-07-11 23:41:45 +0200

    Tablefy poťadí

--- components/poradie.php
+++ components/poradie.php
@@ -1,4 +1,4 @@
-<div class="col-xs-12 col-md-6">
+<div class="col-xs-12 col-md-12">
 	<div class="panel panel-primary">
 		<div class="panel-heading" data-toggle="collapse" href="#poradi" style="cursor: pointer">
 				<h1 class="panel-title">Poradie</h1>
@@ -9,7 +9,7 @@
 	$dotaz = 'SELECT jmeno,body FROM hraci';
 	$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 	while ($zaznam = mysql_fetch_array($vysledek)){
-		echo'<li data-body="'.$zaznam['body'].'">'.$zaznam['body']." ".$zaznam['jmeno'].'</li>';
+		echo'<li data-body="'.$zaznam['body'].'"><span>'.$zaznam['body']."</span> <span>".$zaznam['jmeno'].'</span></li>';
 	}
 ?>
 			</ol>
