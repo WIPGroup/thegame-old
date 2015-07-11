7fb35b65c9cab4e18b5d7f2776892019d41aee75
Václav Jančařík <vaclav.janc@gmail.com>
2015-07-11 23:26:25 +0200

    Lepší schovávání

--- components/poradie.php
+++ components/poradie.php
@@ -1,15 +1,15 @@
 <div class="col-xs-12 col-md-12">
 	<div class="panel panel-primary">
-		<div class="panel-heading" data-toggle="collapse" href="#inv" style="cursor: pointer">
+		<div class="panel-heading" data-toggle="collapse" href="#poradi" style="cursor: pointer">
 				<h1 class="panel-title">Inventár</h1>
 		</div>
-		<div class="panel-body panel-collapse collapse in">
+		<div class="panel-body panel-collapse collapse in" id="poradi">
 			<ol id="poradie">
 <?php
 	$dotaz = 'SELECT jmeno,body FROM hraci';
 	$vysledek = mysql_query($dotaz) or die(mysql_error($db));
 	while ($zaznam = mysql_fetch_array($vysledek)){
-		echo'<li data-body="'.$zaznam['body'].'">'.$zaznam['body'].$zaznam['jmeno'].'</li>';
+		echo'<li data-body="'.$zaznam['body'].'">'.$zaznam['body']." ".$zaznam['jmeno'].'</li>';
 	}
 ?>
 			</ol>
