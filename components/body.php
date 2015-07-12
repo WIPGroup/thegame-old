c4af01d0b1170a93e8ce52cb9c2f7d9386c31296
Jiří Vozár <jiri.vozar@gmail.com>
2015-07-12 14:12:35 +0200

    Zobrazování výkonu za sekudnu.

--- components/body.php
+++ components/body.php
@@ -5,6 +5,17 @@
 $dotaz = 'SELECT * FROM hraci WHERE idhrace='.$_SESSION['hrac'];
 $vysledek = mysql_query($dotaz) or die(mysql_error($db));
 $hrac = mysql_fetch_array($vysledek);
+
+$bodys = 0; $vyzkums = 0;
+$dotaz = 'SELECT * FROM sestavy WHERE hrac='.$hrac['idhrace'];
+$vysledek = mysql_query($dotaz) or die(mysql_error($db));
+while ($zaznam = mysql_fetch_array($vysledek))
+{
+	if ($zaznam['vyzkum'] == 0)
+		$bodys += $zaznam['vykon'];
+	else
+		$vyzkums += $zaznam['vykon'];
+}
 ?>
 <div class="col-xs-12 col-md-4">
 	<div class="panel panel-primary">
@@ -13,7 +24,8 @@ $hrac = mysql_fetch_array($vysledek);
 		</div>
 		<div class="panel-body" style="width: 100%; heigth: 100%">
 			<?php
-			echo '<p>Výskumné body: '.number_format($hrac['vyzkum'], 0, '', ' ').'</p><p><strong>Výherné body:</strong> '.number_format($hrac['body'], 0, '', ' ').'</p>';
+			echo '<p>Výskumné body: '.number_format($hrac['vyzkum'], 0, '', ' ').' ('.$vyzkums.' za sekundu)</p>';
+			echo '<p><strong>Výherné body:</strong> '.number_format($hrac['body'], 0, '', ' ').' ('.$bodys.' za sekundu)</p>';
 			?>
 		</div>
 	</div>
