8e2cbed03c85aa9fc4f5f3b4e05e093817c4ff16
Jozef Hollý <j2.00ghz@gmail.com>
2015-07-12 23:41:29 +0000

    fix names

--- components/footer.php
+++ components/footer.php
@@ -2,7 +2,7 @@
   <div class="panel panel-default">
 	 <div class="panel-body">
 		<p class="text-muted">Copyright © All Rights Reserved</p>
-		<p class="text-muted">Jozef Hollý, Václav Jančařík, Ľuboš Lendáč, Jiří Vozár</p>
+		<p class="text-muted">Martin Borovansky (Data), Jozef Hollý (HTML), Václav Jančařík (JS), Ľuboš Lendáč (Supervision), Jiří Vozár (PHP)</p>
 	 </div>
   </div>
 </footer>
