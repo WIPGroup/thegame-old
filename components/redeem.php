9513173b6d1d212b9840d99ba01af7ae62e524c2
J2ghz <j2.00ghz@gmail.com>
2015-07-13 19:00:18 +0200

    log bad voucher

--- components/redeem.php
+++ components/redeem.php
@@ -40,5 +40,7 @@ if (isset($_GET['kupon']))
 		mysql_query($dotaz);
 	}
 	else
+		$dotaz = 'INSERT INTO log (cas, hrac, text) VALUES ('.time().', '.$_SESSION['hrac'].', "Použitý neplatný kupón <kbd>'.$zaznam['kod'].'</kbd>. Brute Force?';
+		mysql_query($dotaz);
 		echo 'Tento kupón neexistuje.';
 }
